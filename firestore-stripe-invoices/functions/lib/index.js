01944317cd6e8d68c09f5fe6e97df7ff31c15cf5
Thorsten Schaeff <thorsten@stripe.com>
2020-07-21 02:15:33 +0800

    Update version to 0.1.3

--- firestore-stripe-invoices/functions/lib/index.js
+++ firestore-stripe-invoices/functions/lib/index.js
@@ -49,7 +49,7 @@ const stripe = new stripe_1.default(config_1.default.stripeSecretKey, {
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
         name: 'Firebase firestore-stripe-invoices',
-        version: '0.2.0',
+        version: '0.1.3',
     },
 });
 admin.initializeApp();
