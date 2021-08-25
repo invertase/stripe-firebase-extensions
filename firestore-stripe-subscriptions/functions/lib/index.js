dac2ebbb35758f2058d0550660cda2de2728cecd
Darren Ackers <ackers86@hotmail.com>
2021-08-25 05:02:35 +0100

    fix(firestore-stripe-subscriptions): added auto defaults for customer updates involving tax id collections (#235)
    * fix(firestore-stripe-subscriptions) added auto defaults for customer updates involving tax id collections
    
    * updated tax id collection customer_update to obj asignment

--- firestore-stripe-subscriptions/functions/lib/index.js
+++ firestore-stripe-subscriptions/functions/lib/index.js
@@ -178,6 +178,9 @@ exports.createCheckoutSession = functions.firestore
             sessionCreateParams.tax_id_collection = {
                 enabled: true,
             };
+            sessionCreateParams.customer_update = {
+                name: 'auto',
+            };
         }
         if (promotion_code) {
             sessionCreateParams.discounts = [{ promotion_code }];
--- firestore-stripe-subscriptions/functions/src/index.ts
+++ firestore-stripe-subscriptions/functions/src/index.ts
@@ -182,6 +182,9 @@ exports.createCheckoutSession = functions.firestore
         sessionCreateParams.tax_id_collection = {
           enabled: true,
         };
+        sessionCreateParams.customer_update = {
+          name: 'auto',
+        };
       }
       if (promotion_code) {
         sessionCreateParams.discounts = [{ promotion_code }];
