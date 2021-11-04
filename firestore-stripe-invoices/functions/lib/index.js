1fc755922f0fee9b40b6c6837e15aa03715e301e
Lemuel Cardenas <lemuel@stripe.com>
2021-11-03 17:40:08 -0700

    fixed linter error

--- firestore-stripe-invoices/functions/lib/index.js
+++ firestore-stripe-invoices/functions/lib/index.js
@@ -147,7 +147,7 @@ exports.sendInvoice = functions.handler.firestore.document.onCreate(async (snap,
             idempotencyKey: eventId,
             default_tax_rates: payload.default_tax_rates,
             transfer_data: payload.transfer_data,
-            description: payload.description
+            description: payload.description,
         });
         if (invoice) {
             // Email the invoice to the customer
