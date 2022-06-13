9693525b21b0b050b554cf669461db008bc29cfe
dackers86 <ackers86@hotmail.com>
2022-06-13 23:08:05 +0100

    feat(firestore-stripe-payments): upgraded to the new emulator suite

--- firestore-stripe-payments/functions/__tests__/run-script.ts
+++ firestore-stripe-payments/functions/__tests__/run-script.ts
@@ -8,7 +8,7 @@ import { setupProxy, cleanupProxy } from './helpers/setupProxy';
   const { result } = await concurrently(
     [
       {
-        command: 'npm run test:ci',
+        command: 'npm run exec:emulator',
         name: 'testing',
       },
     ],
