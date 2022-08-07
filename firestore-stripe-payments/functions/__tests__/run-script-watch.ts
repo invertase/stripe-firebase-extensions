d5d85a8b033e6287ae3af5458ee61cc2b60717f7
dackers86 <ackers86@hotmail.com>
2022-08-07 21:18:18 +0100

    chore(firestore-stripe-payments): removed obsolete comments

--- firestore-stripe-payments/functions/__tests__/run-script-watch.ts
+++ firestore-stripe-payments/functions/__tests__/run-script-watch.ts
@@ -3,11 +3,6 @@ const concurrently = require('concurrently');
 import { setupProxy, cleanupProxy } from './helpers/setupProxy';
 
 (async () => {
-  /** Clear all webhooks with ngrok.io,
-   * useful for clearing any failed ci testing
-   */
-  // await cleanupAllWebhooks();
-
   const proxyId = await setupProxy();
 
   const { result } = await concurrently(
