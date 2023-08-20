0462066053571158612827321da5c1d1be0199d2
Darren Ackers <ackers86@hotmail.com>
2023-08-20 22:33:32 +0100

    chore(*): update appinfo

--- firestore-stripe-payments/functions/src/index.ts
+++ firestore-stripe-payments/functions/src/index.ts
@@ -34,8 +34,8 @@ const stripe = new Stripe(config.stripeSecretKey, {
   // Register extension as a Stripe plugin
   // https://stripe.com/docs/building-plugins#setappinfo
   appInfo: {
-    name: 'Firebase firestore-stripe-payments',
-    version: '0.3.3',
+    name: 'Firebase Invertase firestore-stripe-payments',
+    version: '0.3.6',
   },
 });
 
