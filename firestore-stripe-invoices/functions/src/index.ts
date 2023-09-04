412c6f8d6ae0894bef822c3e23846b0360a0f158
kazuwombat <matsumotokazuya7@gmail.com>
2023-09-05 06:59:59 +0900

    chore(firestore-stripe-invoices): Tiny refactoring

--- firestore-stripe-invoices/functions/src/index.ts
+++ firestore-stripe-invoices/functions/src/index.ts
@@ -74,7 +74,7 @@ const createInvoice = async function ({
     );
 
     // Create the individual invoice items for this customer from the items in payload
-    const items: Array<Stripe.InvoiceItem> = await Promise.all(itemPromises);
+    await Promise.all(itemPromises);
 
     const invoiceCreateParams: Stripe.InvoiceCreateParams = {
       customer: customer.id,
@@ -255,7 +255,7 @@ export const updateInvoice = functions.handler.https.onRequest(
 
     logs.startInvoiceUpdate(eventType);
 
-    let invoicesInFirestore = await admin
+    const invoicesInFirestore = await admin
       .firestore()
       .collection(config.invoicesCollectionPath)
       .where('stripeInvoiceId', '==', invoice.id)
