b80c12811e8a6ef0cae8721daed74b84d0a97499
Darren Ackers <ackers86@hotmail.com>
2023-09-11 08:41:57 +0100

    chore: testing

--- firestore-stripe-payments/_emulator/functions/index.js
+++ firestore-stripe-payments/_emulator/functions/index.js
@@ -7,9 +7,6 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
