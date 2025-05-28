67e28ea714f0b92b04324bd1dc67ea0cc2fb6821
Jacob Cable <jacobcable94@gmail.com>
2025-05-28 16:41:45 +0100

    fix: output builds for all syntax with tsup

---
+++ firestore-stripe-web-sdk/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  treeshake: true,
+  outDir: "lib",
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    };
+  },
+  esbuildOptions(options) {
+    options.define = {
+      __VERSION__: JSON.stringify(require("./package.json").version),
+    };
+  },
+});
