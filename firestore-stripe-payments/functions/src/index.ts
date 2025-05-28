6c144b1ce960df6357340b2e1d2634f235172498
Jacob Cable <jacobcable94@gmail.com>
2025-05-28 13:26:59 +0100

    refactor: extract createCustomerRecord

--- firestore-stripe-payments/functions/src/index.ts
+++ firestore-stripe-payments/functions/src/index.ts
@@ -29,6 +29,7 @@ import * as logs from './logs';
 import config from './config';
 import { Timestamp } from 'firebase-admin/firestore';
 import { stripe, apiVersion } from './config';
+import { createCustomerRecord } from './utils';
 
 admin.initializeApp();
 
@@ -37,51 +38,6 @@ const eventChannel =
   getEventarc().channel(process.env.EVENTARC_CHANNEL, {
     allowedEventTypes: process.env.EXT_SELECTED_EVENTS,
   });
-
-/**
- * Create a customer object in Stripe when a user is created.
- */
-const createCustomerRecord = async ({
-  email,
-  uid,
-  phone,
-}: {
-  email?: string;
-  phone?: string;
-  uid: string;
-}) => {
-  try {
-    logs.creatingCustomer(uid);
-    const customerData: CustomerData = {
-      metadata: {
-        firebaseUID: uid,
-      },
-    };
-    if (email) customerData.email = email;
-    if (phone) customerData.phone = phone;
-    const customer = await stripe.customers.create(customerData);
-
-    // Add a mapping record in Cloud Firestore.
-    const customerRecord = {
-      email: customer.email,
-      stripeId: customer.id,
-      stripeLink: `https://dashboard.stripe.com${
-        customer.livemode ? '' : '/test'
-      }/customers/${customer.id}`,
-    };
-    if (phone) (customerRecord as any).phone = phone;
-    await admin
-      .firestore()
-      .collection(config.customersCollectionPath)
-      .doc(uid)
-      .set(customerRecord, { merge: true });
-    logs.customerCreated(customer.id, customer.livemode);
-    return customerRecord;
-  } catch (error) {
-    logs.customerCreationError(error, uid);
-    return null;
-  }
-};
 
 exports.createCustomer = functions.auth
   .user()
